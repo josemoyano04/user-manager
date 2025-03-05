0e8d73534ee2c29dbd6fdf6eff1b5d319632a1e7
Jose <josemoyano059@gmail.com>
2025-03-04 23:33:13 -0300

    Agregar middleware CORS y actualizar dependencias en requirements.txt

--- main.py
+++ main.py
@@ -1,7 +1,14 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from routers import authentication_routers as atr, users_router as ur
 
 app = FastAPI()
-
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["GET", "POST", "PUT", "DELETE"], 
+    allow_headers=["*"],  # Permite todos los encabezados, cambia según tus necesidades
+)
 app.include_router(atr.router)
 app.include_router(ur.router)
