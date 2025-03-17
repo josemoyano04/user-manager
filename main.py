65e8054bad63610ba5d9247f702cff6d97c9dc9c
Jose <josemoyano059@gmail.com>
2025-03-17 16:24:58 -0300

    Metadatos en instancia de FastApi

--- main.py
+++ main.py
@@ -2,13 +2,21 @@ from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from routers import authentication_routers as atr, users_router as ur
 
-app = FastAPI()
+app = FastAPI(
+    title= "User Manager API", 
+    version= "0.0.1",
+    contact= {
+        "name": "Jose Moyano",
+        "url": "http://www.github.com/josemoyano04",
+        "email": "josemoyano059@gmail.com",
+    })
+
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["GET", "POST", "PUT", "DELETE"], 
-    allow_headers=["*"],  # Permite todos los encabezados, cambia según tus necesidades
+    allow_headers=["*"]
 )
 app.include_router(atr.router)
 app.include_router(ur.router)
